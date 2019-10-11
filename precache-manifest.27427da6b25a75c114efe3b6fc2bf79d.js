4b041a8279d363ad327347714d19aa1530210201
Nathan Fiscus <nathan_fiscus@hotmail.com>
2019-10-10 23:01:35 -0600

    Updates

---
+++ precache-manifest.27427da6b25a75c114efe3b6fc2bf79d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ba02d95fc38021293b5583993ee22b0",
+    "url": "/inovelli-notfication-calc/index.html"
+  },
+  {
+    "revision": "30aa176b54ea24c562be",
+    "url": "/inovelli-notfication-calc/static/js/2.3c71871a.chunk.js"
+  },
+  {
+    "revision": "183d548714f51621661e",
+    "url": "/inovelli-notfication-calc/static/js/main.97ecdccb.chunk.js"
+  },
+  {
+    "revision": "c7422a4114b35d7b0d22",
+    "url": "/inovelli-notfication-calc/static/js/runtime-main.37d845d3.js"
+  },
+  {
+    "revision": "b878040d8a8e1d83ec289a89bbf43236",
+    "url": "/inovelli-notfication-calc/static/media/Inovelli-LZW30-SN.b878040d.gif"
+  }
+]);
