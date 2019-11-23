e153301486057be819a9d77d5a917da2a0a7e585
Nathan Fiscus <nathan_fiscus@hotmail.com>
2019-11-22 21:02:52 -0700

    Updates

--- precache-manifest.f8d5135e44cc3bbeff11cbe89b20b050.js
+++ precache-manifest.f8d5135e44cc3bbeff11cbe89b20b050.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2bdcac6c367178da1f372a175dc0b064",
+    "revision": "7fa93d290c5244075befbfcf351c0e76",
     "url": "/inovelli-notification-calc/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/inovelli-notification-calc/static/js/2.cb4a1580.chunk.js"
   },
   {
-    "revision": "ad8f001b87d2b9af65c6",
-    "url": "/inovelli-notification-calc/static/js/main.6d8eeb71.chunk.js"
+    "revision": "a23faa98f1ec7f1c681c",
+    "url": "/inovelli-notification-calc/static/js/main.be48049b.chunk.js"
   },
   {
     "revision": "76902d835905de61ea91",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/inovelli-notification-calc/precache-manifest.620e95a6dd246bf016825c20edff6268.js"
+  "/inovelli-notification-calc/precache-manifest.f8d5135e44cc3bbeff11cbe89b20b050.js"
 );
 
 self.addEventListener('message', (event) => {
