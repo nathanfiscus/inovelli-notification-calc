017752d45bee404811c60b3daae23de04bada817
Nathan Fiscus <nathan_fiscus@hotmail.com>
2019-12-12 16:30:26 -0700

    Switch Icon on Scene Table

--- src/SceneRow.js
+++ src/SceneRow.js
@@ -5,9 +5,10 @@ import {
   withStyles,
   IconButton,
   Menu,
-  MenuItem
+  MenuItem,
+  SvgIcon,
+  Tooltip
 } from "@material-ui/core";
-import MoreVertIcon from "@material-ui/icons/MoreVert";
 import YAML from "json-to-pretty-yaml";
 import copyToClipboard from "./ClipboardAccess";
 
@@ -131,9 +132,15 @@ class SceneRow extends React.PureComponent {
               : undefined
           }
         >
-          <IconButton size="small" onClick={this.toggleMenu}>
-            <MoreVertIcon />
-          </IconButton>
+          <Tooltip title="Copy to Clipboard">
+            <IconButton size="small" onClick={this.toggleMenu}>
+              <SvgIcon>
+                <svg viewBox="0 0 24 24">
+                  <path d="M19,21H8V7H19M19,5H8A2,2 0 0,0 6,7V21A2,2 0 0,0 8,23H19A2,2 0 0,0 21,21V7A2,2 0 0,0 19,5M16,1H4A2,2 0 0,0 2,3V17H4V3H16V1Z" />
+                </svg>
+              </SvgIcon>
+            </IconButton>
+          </Tooltip>
           <Menu
             open={Boolean(this.state.anchor)}
             anchorEl={this.state.anchor}
